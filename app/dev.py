f2517aecdfdbe05970023a8f042ceca64a3c1e5e
dylanhan99 <dylanhan99@gmail.com>
2025-03-31 11:01:27 +0800

    kkb DB stuff

--- app/dev.py
+++ app/dev.py
@@ -6,11 +6,36 @@ import ofpdb
 
 blueprint_dev = Blueprint("dev", __name__)
 
+def set_selected_table(table_name: str):
+    selected_table = ofpdb.get_table(table_name)
+    if selected_table is None:
+        session["selected_table_name"] = "-"
+        session["selected_table_col_names"] = None
+        session["selected_table_data"] = None
+        return False, "ofpdb Failed to get_table"
+    
+    succ, result = ofpdb.execute_sql_query(selected_table.select())
+    if not succ:
+        session["selected_table_name"] = "-"
+        session["selected_table_col_names"] = None
+        session["selected_table_data"] = None
+        return False, f"ofpdb Failed to execute_sql_query '{result}'"
+    
+    keys = [column.name for column in selected_table.columns]
+    print (result)
+    rows = [dict(row) for row in result]
+    session["selected_table_name"] = table_name
+    session["selected_table_col_names"] = keys
+    session["selected_table_data"] = rows
+    return True, ""
+
 def render_dev():
     return render_template('dev.html', \
                             public_ipv4=current_app.config["IPV4_PUBLIC"], \
                             table_ddl=session.get("table_ddl", []), \
                             selected_table_name=session.get("selected_table_name", "-"), \
+                            selected_table_col_names=session.get("selected_table_col_names", None), \
+                            selected_table_data=session.get("selected_table_data", None), \
                             query_err_msg=session.get("query_err_msg", "") \
                         )
 
@@ -19,6 +44,7 @@ def dev_index():
     query = "SELECT name FROM sqlite_master WHERE type='table'"
     succ, sql_table_names = ofpdb.execute_sql_query(query)
     
+    print(f"sql_table_names > {sql_table_names}")
     session["table_ddl"] = [row[0] for row in sql_table_names] if succ else []
 
     return render_dev()
@@ -27,9 +53,11 @@ def dev_index():
 def dev_submit():
     if request.method == 'POST':
         form_id = request.form.get("form_id")
-        print(form_id)
         if form_id == "choose_table":
-            session["selected_table_name"] = request.form["table_ddl"]
+            table_name = request.form["table_ddl"]
+            select_ok, err_msg = set_selected_table(table_name)
+            if not select_ok:
+                print(f"DEV - Failed to get {table_name} from ofdb > '{err_msg}'")
             
         elif form_id == "submit_query":
             query = request.form["query"]
--- app/ofpdb.py
+++ app/ofpdb.py
@@ -1,16 +1,33 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import Executable, create_engine, Table, MetaData, inspect, text
 from sqlalchemy.exc import SQLAlchemyError
 
 db_engine = None
+db_metadata = None
 
 def init():
     global db_engine
+    global db_metadata
     db_engine = create_engine("sqlite:///ofp.db")
+    db_metadata = MetaData()
 
 def execute_sql_query(query):
     try:
         with db_engine.connect() as connection:
-            result = connection.execute(text(query))
+            if isinstance(query, Executable):
+                result = connection.execute(query)
+            elif isinstance(query, str):
+                result = connection.execute(text(query))
+            else:
+                return False, "execute_sql_query - Query is neither a Executable nor a str"
             return True, result.fetchall()
     except SQLAlchemyError as e:
         return False, str(e)
+
+# Returns a handle to the table. 
+# Returns None if cannot find.    
+def get_table(table_name: str):
+    print(f"get_table - {table_name}")
+    db_inspector = inspect(db_engine)
+    if table_name not in db_inspector.get_table_names():
+        return None
+    return Table(table_name, db_metadata, autoload_with=db_engine)
